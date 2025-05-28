47280e9e340cb66ce3d7e4717da9915e2afdbd10
mahamudsourov <sourovm554@gmail.com>
2025-05-28 12:58:44 +0600

    added_navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,61 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body{
+            padding: 0;
+            margin: 0;
+        }
+         .disp-flex{
+            display: flex;
+         }
+         .flex-justify-space{
+            justify-content: space-between;
+         }
+         .p{
+              padding: 10px;
+         }
+         nav{
+            background-color: aqua;
+         }
+         .no-decor{
+             text-decoration: none;
+         }
+         
+         
+    </style>
 </head>
 <body>
-    Welcome php
+    <nav>
+        <div class="disp-flex flex-justify-space">
+            <div class="logo"><p>Sourov</p> </div>
+            <div class="p">
+                <a class="no-decor" href="#">About</a>
+                <a class="no-decor"  href="#">Research Project</a>
+                <a class="no-decor"  href="#">Blog</a>
+                <a class="no-decor"  href="#">Contact</a>
+                <a class="no-decor"  href="#">Linked In</a>
+                <a class="no-decor"  href="#">Git</a>
+            </div>  
+        </div>
+    </nav>
+    <section>
+        <div class="disp-flex flex-justify-space">
+            <div class="img1">
+                <img src="{{ asset('assets/images/p1.jpg')}}" alt="">
+            </div>
+            <div>
+                about
+            </div>
+        </div>
+        <div class="disp-flex flex-justify-space">
+            <div>
+                 Portfolio
+            </div>
+            <div>
+                My projects
+            </div>
+        </div>
+    </section>
 </body>
 </html>
