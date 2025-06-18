31d3a480ef446eebed8a8ca6689e581eaa74e75f
mahamudsourov <sourovm554@gmail.com>
2025-06-18 11:26:38 +0600

    jsEvents

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,52 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        body{
+        body {
             padding: 0;
             margin: 0;
         }
-         .disp-flex{
+
+        .disp-flex {
             display: flex;
-         }
-         .flex-justify-space{
+        }
+
+        .flex-justify-space {
             justify-content: space-between;
-         }
-         .p{
-              padding: 10px;
-         }
-         nav{
+        }
+
+        .p {
+            padding: 10px;
+        }
+
+        nav {
             background-color: aqua;
-         }
-         .no-decor{
-             text-decoration: none;
-         }
-         
-         
+        }
+
+        .no-decor {
+            text-decoration: none;
+        }
     </style>
 </head>
+
 <body>
     <nav>
         <div class="disp-flex flex-justify-space">
-            <div class="logo"><p>Sourov</p> </div>
-            <div class="p">
+            <div class="logo">
+                <p>Sourov</p>
+            </div>
+            <div class="nav-links">
                 <a class="no-decor" href="#">About</a>
-                <a class="no-decor"  href="#">Research Project</a>
-                <a class="no-decor"  href="#">Blog</a>
-                <a class="no-decor"  href="#">Contact</a>
-                <a class="no-decor"  href="#">Linked In</a>
-                <a class="no-decor"  href="#">Git</a>
-            </div>  
+                <a class="no-decor" href="#">Research Project</a>
+                <a class="no-decor" href="#">Blog</a>
+                <a class="no-decor" href="#">Contact</a>
+                <a class="no-decor" href="#">Linked In</a>
+                <a class="no-decor" href="#">Git</a>
+            </div>
         </div>
     </nav>
     <section>
         <div class="disp-flex flex-justify-space">
             <div class="img1">
-                <img src="{{ asset('assets/images/p1.jpg')}}" alt="">
+                <img src="{{ asset('assets/images/p1.jpg') }}" alt="">
             </div>
             <div>
                 about
@@ -54,12 +61,19 @@
         </div>
         <div class="disp-flex flex-justify-space">
             <div>
-                 Portfolio
+                Portfolio
             </div>
             <div>
                 My projects
             </div>
         </div>
+        <div>
+            <h1 class="title">Hello I am Sourov</h1>
+            <p id="test">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, reprehenderit.</p>
+        </div>
     </section>
+
+    <script src="{{ asset('assets/script.js') }}"></script>
 </body>
+
 </html>
